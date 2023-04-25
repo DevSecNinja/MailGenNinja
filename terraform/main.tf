9e8d695f4eb77fdfd87b3fa1f503154e324f152d
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2023-04-25 10:05:50 +0200

    Add Azure CAF provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.29.1"
     }
+    
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "2.0.0-preview3"
+    }
   }
 
   cloud {
