9f2e9ca29701baff3b7f65d5442d118656387cd0
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2023-04-26 10:34:27 +0000

    Fix config

--- mailgenninja/src/msalConfig.js
+++ mailgenninja/src/msalConfig.js
@@ -4,8 +4,7 @@ const msalConfig = {
     auth: {
         clientId: process.env.REACT_APP_AZURE_AD_APP_CLIENT_ID,
         authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_AD_APP_TENANT_ID}`,
-        // redirectUri: window.location.origin,
-        redirectUri: "https://agreeable-grass-0ecf41e03.3.azurestaticapps.net",
+        redirectUri: window.location.origin,
     },
 };
 
